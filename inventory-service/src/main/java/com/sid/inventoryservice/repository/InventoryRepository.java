c6a6b4b50b502eaab32c42672e36ca419f0e232a
redae <reda.elmallahi@gmail.com>
2023-06-01 21:41:50 +0100

    app-docker.properties commit

--- inventory-service/src/main/java/com/sid/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/sid/inventoryservice/repository/InventoryRepository.java
@@ -2,10 +2,12 @@ package com.sid.inventoryservice.repository;
 
 import com.sid.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     List<Inventory> findBySkuCodeIn(List<String> skuCode);
 
--- order-service/src/main/java/com/sid/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/sid/orderservice/repository/OrderRepository.java
@@ -2,10 +2,12 @@ package com.sid.orderservice.repository;
 
 import com.sid.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * The interface Order repository.
  */
+@Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
 }
